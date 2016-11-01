c427e078851246d29487f2aeeb3c8c8b2398ceb7
jbedia <joaquin.bedia@gmail.com>
2016-11-01 21:34:35 +0100

    Doc update

--- R/interpGrid.R
+++ R/interpGrid.R
@@ -1,3 +1,20 @@
+##     interpGrid.R Grid interpolation
+##
+##     Copyright (C) 2016 Santander Meteorology Group (http://www.meteo.unican.es)
+##
+##     This program is free software: you can redistribute it and/or modify
+##     it under the terms of the GNU General Public License as published by
+##     the Free Software Foundation, either version 3 of the License, or
+##     (at your option) any later version.
+## 
+##     This program is distributed in the hope that it will be useful,
+##     but WITHOUT ANY WARRANTY; without even the implied warranty of
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     GNU General Public License for more details.
+## 
+##     You should have received a copy of the GNU General Public License
+##     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #' @title Grid interpolation
 #' @description Interpolation of gridded datasets into a user-defined grid using nearest-neighbour or bilinear weights. 
 #' @importFrom akima interp
