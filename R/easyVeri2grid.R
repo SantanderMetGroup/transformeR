7f2f3003479cc7c6d0e8c4c4cc4ca66f0ff9d886
jbedia <joaquin.bedia@gmail.com>
2017-05-15 11:40:11 +0200

    Add new function for p-value estimation

--- R/easyVeri2grid.R
+++ R/easyVeri2grid.R
@@ -1,6 +1,6 @@
 #     easyVeri2grid easyVerification matrix to climatological grid conversion
 #
-#     Copyright (C) 2016 Santander Meteorology Group (http://www.meteo.unican.es)
+#     Copyright (C) 2017 Santander Meteorology Group (http://www.meteo.unican.es)
 #
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
@@ -46,4 +46,28 @@ easyVeri2grid <- function(easyVeri.mat, obs.grid, verifun = NULL) {
       attr(obs.grid[["Data"]], "climatology:fun") <- clim.att
       return(obs.grid)
 }
+
+
+#' @title Identify significant verification points
+#' @description Two-tailed alternative hypothesis testing for \pkg{easyVerification} "sigma" outputs
+#' @param ref.mean Numeric of length one. Null hypothesis reference mean (e.g. 0.5 for AUC tests, 0 for skill scores)
+#' @param est.mean easyVerification output matrix with scores/measures. This is the \code{cat[num]} element of the \code{veriApply} output
+#' @param sigma Standard deviation of the estimated mean. This is the \code{cat[num].sigma} element of the \code{veriApply} output.
+#' @param n Sample mean (number of ensemble members)
+#' @return A easyVerification-like matrix of p-values. Ready to be passed to \code{\link{easyVeri2grid}}
+#'  for climatological grid conversion, and possibly to \code{\link{map.stippling}} after that, to depict significant points
+#'  in a verification map.
+#' @importFrom stats pnorm 
+#' @author J Bedia
+#' @export
+#' @seealso \code{\link{easyVeri2grid}}, for conversion of verification matrices into climatological grids.
+#'  Also, \code{\link{map.stippling}}, to draw significant points on verification maps.
+
+easyVeri.pval <- function(ref.mean, est.mean, sigma, n) {
+    z <- (est.mean - ref.mean) / (sigma / sqrt(n))
+    pval <- z
+    pval[z < 0 & !is.na(z)] <- 2 * pnorm(z[z < 0 & !is.na(z)])
+    pval[z >= 0 & !is.na(z)] <- 2 * (1 - pnorm(z[z >= 0 & !is.na(z)]))
+}
+
 
