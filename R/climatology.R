e31636bf9b5a242f8867d8da1ce9280e56ea37f4
jbedia <joaquin.bedia@gmail.com>
2017-05-15 09:28:37 +0200

    New function `trend.1D` for linear trend analysis

--- R/climatology.R
+++ R/climatology.R
@@ -81,21 +81,15 @@ climatology <- function(grid,
                                       parallel,
                                       max.ncores,
                                       ncores)
-            dimNames <- attr(grid[["Data"]], "dimensions")
+            dimNames <- getDim(grid)
       }
       mar <- grep("^time$", dimNames, invert = TRUE)
       if (length(dimNames) == length(mar)) stop("Time dimension not found", call. = FALSE)
       arg.list <- c(clim.fun, list("MARGIN" = mar), list("X" = grid[["Data"]]))
-      clim <- if (parallel.pars$hasparallel) {
-            message("[", Sys.time(), "] - Computing climatology in parallel...")
-            arg.list[["cl"]] <- parallel.pars$cl
-            on.exit(parallel::stopCluster(parallel.pars$cl))
-            do.call("parApply", arg.list)      
-      } else {
-            message("[", Sys.time(), "] - Computing climatology...")
-            apply(grid[["Data"]], MARGIN = mar, FUN = "mean", na.rm = TRUE)      
-            do.call("apply", arg.list)      
-      }
+      pply_fun <- selectPar.pplyFun(parallel.pars, .pplyFUN = "apply")
+      if (parallel.pars$hasparallel) on.exit(parallel::stopCluster(parallel.pars$cl))
+      message("[", Sys.time(), "] - Computing climatology...")      
+      clim <- do.call("pply_fun", arg.list)
       message("[", Sys.time(), "] - Done.")
       clim <- abind(clim, along = -1L)
       dimNames.aux <- c("time", dimNames[-grep("^time", dimNames)])
@@ -106,12 +100,62 @@ climatology <- function(grid,
       grid[["Data"]] <- unname(clim)
       ## Attributes of Data
       attr(grid$Data, "dimensions") <- dimNames
-      attr(grid$Data, "climatology:fun") <- clim.fun[["FUN"]]
       ## Date adjustment
       attr(grid$Dates, "season") <- getSeason(grid)
       grid$Dates$start <- grid$Dates$start[1]
       grid$Dates$end <- tail(grid$Dates$end, 1)
+      grid <- redim(grid, drop = TRUE) %>% redim(member = FALSE, var = FALSE, drop = FALSE)
+      attr(grid$Data, "climatology:fun") <- clim.fun[["FUN"]]
       return(grid)
 }
 
 
+#' @title Linear trend analysis
+#' @description Atomic function for (linear) trend analysis
+#' @param ts Numeric vector containing a time series
+#' @param dates Dates vector associated to the records in \code{ts}.
+#'  Standard format, as returned for instance by \code{\link{getRefDates}}
+#' @param method Correlation method. Passed to \code{\link{cor.test}}.
+#' @param return.pvalue Logical. Should p-values be returned instead of correlation coefficient estimates?.
+#' Default to \code{FALSE}. 
+#' @param conf.level Numeric value indicating the confidence level for the significance test.
+#'  Passed to \code{\link{cor.test}}.
+#' @details The function can be conveniently wrapped using \code{climatology}. See examples.
+#' @export
+#' @importFrom stats cor.test
+#' @author J Bedia
+#' @seealso \code{\link{climatology}}, for computing climatological trend maps from grids
+#' @examples 
+#' # Simulate a positive trend
+#' set.seed(1)
+#' ts <- runif(min = 21, max = 23.5, n = 100) + seq(.1, .15, length.out = 100) * 10
+#' dates <- paste0(1901:2000,"-01-01 00:00:00 GMT")
+#' plot(1901:2000, ts, ty = 'l', ylab = "Simulated Mean Temp (degC)", xlab = "Year")
+#' abline(reg = lm(ts ~ I(1901:2000)), col = "red", lty = 2)
+#' trend.1D(ts, dates, return.pvalue = FALSE) # Default to Pearson's r
+#' trend.1D(ts, dates, return.pvalue = TRUE)
+#' # An example of how to compute a trend map using climatology as a wrapper for trend:
+#' data("EOBS_Iberia_tas")
+#' dates <- getRefDates(EOBS_Iberia_tas)
+#' tau.estimate <- climatology(EOBS_Iberia_tas,
+#'                             clim.fun = list(FUN = "trend.1D", dates = dates, method = "kendall"))
+#' plotClimatology(tau.estimate)
+#' # Adding significant trend points is usually needed: 
+#' pval.estimate <- climatology(EOBS_Iberia_tas,
+#'                              clim.fun = list(FUN = "trend.1D",
+#'                                              dates = dates,
+#'                                              method = "kendall",
+#'                                              return.pvalue = TRUE))
+#' sig.points <- map.stippling(clim = pval.estimate, threshold = 0.05, condition = "LT", 
+#'                             pch = 19, cex = .25, col = "purple")
+#' plotClimatology(tau.estimate, sp.layout = list(sig.points))
+
+trend.1D <- function(ts, dates, method = c("pearson","spearman","kendall"),
+                     return.pvalue = FALSE, conf.level = 0.95) {
+    if (length(dates) != length(ts)) stop("Input and date vector sizes do not match")
+    method <- match.arg(method, c("pearson","spearman","kendall"))
+    x <- as.numeric(as.Date(dates))
+    i <- ifelse(return.pvalue, "p.value", "estimate")
+    tryCatch(cor.test(x = ts, y = x, method = method, conf.level = conf.level)[[i]],
+             error = function(err) NA)
+}
