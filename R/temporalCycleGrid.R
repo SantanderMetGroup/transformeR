85086e7d37e6b82468d50aafb31e8a4cbd6f6810
jbedia <joaquin.bedia@gmail.com>
2017-06-05 22:26:40 +0200

    Update annual cycle example
    Fix output dimensions ordering

--- R/temporalCycleGrid.R
+++ R/temporalCycleGrid.R
@@ -9,19 +9,19 @@
 ## 
 ##     This program is distributed in the hope that it will be useful,
 ##     but WITHOUT ANY WARRANTY; without even the implied warranty of
-##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ##     GNU General Public License for more details.
 ## 
 ##     You should have received a copy of the GNU General Public License
 ##     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #' @title Temporal cycle calculation
-#' @description A function to compute a \dQuote{daily/monthly climatology}. See Details.
+#' @description A function to compute temporal cycles. See Details.
 #' @param grid Input grid. This must be a daily grid, possibly previously filtered. See details
 #' @param time.frame Character string. Temporal unit on which the cycle is based on. Currently acceped values are \code{"daily"}
 #' (the default, requires a daily input grid) or monthly
-#' @param aggr.fun Function used to aggregate the values. Default to \code{\link{mean} }.
-#' @param ... Further arguments passed to \code{aggr.fun} (e.g. \code{na.rm = TRUE}, ...)
+#' @param clim.fun Function used to aggregate the values. Default to \code{\link{mean}}, but can be flexibly defined.
+#' @param ... Further arguments passed to \code{clim.fun} (e.g. \code{na.rm = TRUE}, ...)
 #' @template templateParallelParams
 #' @details This function is intended for the computation of reference daily/ monthly climatologies 
 #' (i.e., a climatological reference for each -julian- day of the year/month). It thus has a maximum temporal 
@@ -36,7 +36,9 @@
 #' 
 #' Daily average climatologies are typically calculated after the application of a moving average filter, thus
 #' reducing the noise introduced by the large inter-annual variability at this temporal resolution. It is therefore
-#' recommended the application of \code{\link{filterGrid}} prior to its usage (See examples).
+#' recommended the application of \code{\link{filterGrid}} (See examples).
+#' 
+#' @return A temporal cycle grid
 #' 
 #' @template templateParallel
 #' @importFrom magrittr %>% 
@@ -47,13 +49,16 @@
 #' @seealso \code{\link{filterGrid}} for the application of moving averages to grids.
 #' @examples 
 #' data("EOBS_Iberia_tas")
+#' 
 #' ## DAILY CYCLE (example with winter EOBS mean temperature)
 #' grid <- EOBS_Iberia_tas
 #' Mn <- temporalCycleGrid(grid, time.frame = "daily")
-# We compute the areal mean to plot the results as a time-series
+#' # We compute the areal mean to plot the results as a time-series
 #' aggr.fun <- list(FUN = "mean", na.rm = TRUE)
 #' Mn.agg <- aggregateGrid(Mn, aggr.lon = aggr.fun, aggr.lat = aggr.fun)
-#' plot(Mn.agg$Data, ty = 'l', ylim = c(0,15), axes = FALSE, xlab = "month-day", ylab = "Mean Temp (degC)")
+#' plot(Mn.agg$Data, ty = 'l', ylim = c(0,15), axes = FALSE,
+#'      xlab = "month-day", ylab = "Mean Temp (degC)",
+#'      main = "Daily Cycle DJF 1997-2000")
 #' axis(2)
 #' axis(1, at = seq(1,length(Mn.agg$Data),5), las = 2,
 #'      labels = substr(getRefDates(Mn.agg), 6, 10)[seq(1,length(Mn.agg$Data),5)])
@@ -66,20 +71,37 @@
 #' mx.agg <- aggregateGrid(mx, aggr.lon = aggr.fun, aggr.lat = aggr.fun, weight.by.lat = TRUE)
 #' # Range envelope
 #' ix <- 1:length(Mn.agg$Data)
-#' polygon(x = c(ix, rev(ix)), y = c(mn.agg$Data, rev(mx.agg$Data)), border = "transparent", col = rgb(.2,.2,.2,.5))
+#' polygon(x = c(ix, rev(ix)), y = c(mn.agg$Data, rev(mx.agg$Data)),
+#'         border = "transparent", col = rgb(.2,.2,.2,.5))
 #' legend("top", c("Daily mean cycle", "range"),
 #'        pch = 22, pt.bg = c("transparent", "grey60"), pt.cex = c(0,2.5),
 #'        lwd = c(1,0), bty = "n")
-#' title(main = "Daily Cycle DJF 1997-2000")
 #' 
 #' ## Smoothing daily data
-#' ## A moving window of 1 week smoothes the daily series prior to annual cycle calculation:
-#' fgrid <- filterGrid(grid, window.width = 7, sides = 2)
-#' fMn <- temporalCycleGrid(fgrid, time.frame = "daily")
+#' ## A moving average window of 1 week is used here to smooth the daily series:
+#' fMn <- filterGrid(Mn, window.width = 7, sides = 2)
 #' fMn.agg <- aggregateGrid(fMn, aggr.lat = aggr.fun, aggr.lon = aggr.fun)
 #' lines(fMn.agg$Data, col = "red")
 #' legend("bottom", "Smoothed daily mean cycle", lty = 1, col = "red", bty = "n")
-
+#' 
+#' ## ANNUAL CYCLE (time.frame = "monthly")
+#' # A monthly input grid is required.
+#' # An attemp to compute a monthly time cycle with daily data will yield an error:
+#' try(temporalCycleGrid(grid, time.frame = "monthly"))
+#' # Thus, monthly aggregation is required:
+#' mgrid <- aggregateGrid(grid, aggr.m = aggr.fun)
+#' mcyc <- temporalCycleGrid(mgrid, time.frame = "monthly")
+#' # The spatial mean is calculated to plot the time series:
+#' mcyc.agg <- aggregateGrid(mcyc, aggr.lat = aggr.fun, aggr.lon = aggr.fun)
+#' plot(mcyc.agg$Data, ty = 'b', xlab = "Month",  ylab = "Mean Temp (degC)",
+#'      axes = FALSE, main = "Monthly Cycle DJF 1997-2000")
+#' axis(2)
+#' axis(1, at = 1:3, labels = month.abb[c(12,1,2)])
+#' ## Also, note the special attributes in $Variable:
+#' # type of cycle:
+#' attributes(mcyc$Variable)$'temporal.cycle::time.frame'
+#' # aggregation function:
+#' attributes(mcyc$Variable)$'temporal.cycle::fun'
 
 temporalCycleGrid <- function(grid, time.frame = c("daily", "monthly"),
                           clim.fun = "mean", ...,
@@ -95,7 +117,9 @@ temporalCycleGrid <- function(grid, time.frame = c("daily", "monthly"),
         grid$Dates  <- lapply(grid$Dates, "unique")
     } else if (time.frame == "monthly") {
         tindex <- getRefDates(grid) %>% as.Date() %>% format("%m") %>% as.integer()
-        if (tindex %>% diff() %>% head(1) == 0) stop("The input grid is not monthly\nConsider the application of 'aggregateGrid' first")
+        if (tindex %>% diff() %>% head(1) == 0) {
+            stop("The input grid is not monthly\nConsider the application of 'aggregateGrid' prior to 'temporalCycleGrid'", call. = FALSE)
+        }
     }
     parallel.pars <- parallelCheck(parallel, max.ncores, ncores)
     pply_fun <- selectPar.pplyFun(parallel.pars, .pplyFUN = "apply")
@@ -103,10 +127,14 @@ temporalCycleGrid <- function(grid, time.frame = c("daily", "monthly"),
     grid <- adjustTemporalCycleDates(grid)
     dimNames <- getDim(grid)
     mar <- grep("^time", dimNames, invert = TRUE)
+    newDim <- c("time", dimNames[mar])
+    message("[", Sys.time(),"] - Calculating temporal cycle...")
     suppressWarnings({
         grid$Data <- pply_fun(grid$Data, MARGIN = mar, FUN = temporalCycle.1D,
                               time.ref.INDEX = tindex, aggr.fun = clim.fun, ...) %>% unname()})
-    attr(grid$Data, "dimensions") <- dimNames
+    message("[", Sys.time(),"] - Done.")
+    attr(grid$Data, "dimensions") <- newDim
+    grid <- redim(grid, drop = TRUE)
     attr(grid$Variable, "temporal.cycle::time.frame") <- time.frame
     attr(grid$Variable, "temporal.cycle::fun") <- clim.fun
     invisible(grid)
@@ -135,7 +163,7 @@ adjustTemporalCycleDates <- function(grid) {
     ref.years <- getYearsAsINDEX(grid)
     grid$Dates$start <- start[ref.years %in% range(ref.years)[1]]
     grid$Dates$end <- end[ref.years %in% range(ref.years)[2]]
-    invisible(grid)
+    return(grid)
 }
 
 
