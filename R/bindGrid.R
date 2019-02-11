3b414cbc9ae0522988098a0ce855a4f6ec30ecab
Your Name <maibide@gmail.com>
2019-02-11 19:21:36 +0100

    bug fix in bindGrid related to loc dimension

--- R/bindGrid.R
+++ R/bindGrid.R
@@ -279,7 +279,7 @@ bindGrid.spatial <- function(..., dimn, tol) {
       lat <- unname(lat)
       grid.list <- NULL
       lats <- do.call(coordfun, lat)
-      if (is.list(lats)) lats <- unlist(lats)
+      if (class(lats) == "list") lats <- unlist(lats)
       attr(ref[["Data"]], "dimensions") <- dimNames
       # n.vars <- getShape(ref, "var")
       #if (n.vars > 1) lats <- rep(list(lats), n.vars)
