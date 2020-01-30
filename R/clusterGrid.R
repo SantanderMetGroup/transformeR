2e173b9c6a1bd763cc260dbdf834d5bdec81227e
juanferngran <juanferngran@gmail.com>
2020-01-30 12:08:22 +0100

    Changes on clusterGrid: members aggregation

--- R/clusterGrid.R
+++ R/clusterGrid.R
@@ -25,12 +25,14 @@
 #'@param ... Further specific arguments passed to the clustering functions 
 #'@seealso \link[stats]{kmeans}, \link[stats]{hclust}, \link[kohonen]{som}.
 #'@importFrom fields rdist
-#'@return A C4R (multimember/multi)grid object that will contain data from: 
+#'@return A C4R (multimember/multi) grid object that will contain data from: 
+#'\itemize{
 #'\item 'grid' input if none 'newdata' or 'y' are passed.  
 #'\item 'newdata' if input 'newdata' is passed and 'y' is not. 
 #'\item 'y' if this predictands grid is passed. 
 #'The clustering type (cluster.type), number of clusters (centers), centroids of clusters from 'grid' input (centroids) and the days corresponding to each 
 #'cluster (index) are returned as attributes in all clustering algorithms. Then, other algorithm-specific parameters are provided as attributes.
+#'}
 #'@details 
 #'\strong{kmeans}
 #'
@@ -53,8 +55,8 @@
 #'
 #'#'\strong{Lamb}
 #'
-#'While using the Lamb Weather Typing algorithm (check \link{LambWT} for further information), the argument 'centers' is not requiered, as it will 
-#'be forced to be equal to 27, according to Jones et al. 2012 (Int J Climatol).
+#'While using the Lamb Weather Typing algorithm (check \link{lambWT} for further information), the argument 'centers' is not requiered, as it will 
+#'be forced to be equal to 26, according to Trigo and daCamara (2000), Int J Climatol. 
 #'@author J. A. Fernandez
 #'@export
 #'@examples 
@@ -86,157 +88,145 @@ clusterGrid <- function(grid,
                         ...) {
  
   type <- match.arg(type, choices = c("kmeans", "hierarchical", "som", "lamb"))
- 
-  #Checking grid dimensions
-  grid <- redim(grid, member = TRUE)
-  n.mem <- getShape(grid, "member")
-  n.var <- suppressMessages(getShape(grid, "var")) 
-  var.names <- getVarNames(grid)
   
-  ### Circulation types
-  #Special case: Lamb WT
-  if (type == "lamb"){
-    grid <- redim(grid, var = TRUE)
-    n.var <- getShape(grid, "var")
-    if ((n.var) != 1){
-      stop("For lamb, only 'psl' variable is admitted. Use subsetGrid to extract it")
+  if (is.null(newdata)){
+    #Checking grid dimensions
+    if (getShape(grid, "member") > 1){
+      message("Clustering analysis will be done after Ensemble mean...")
+      grid <- suppressMessages(aggregateGrid(grid = grid, aggr.mem = list(FUN = "mean", na.rm = TRUE)))
     }
-    if (!is.null(centers)) {
-      message("Lamb WT was choosen, so the number of clusters will be forced to 27. Arg. 'centers' will be ignored.")
+    n.var <- suppressMessages(getShape(grid, "var")) 
+    var.names <- getVarNames(grid)
+    
+    ### Circulation types
+    #Special case: Lamb WT
+    if (type == "lamb"){
+      grid <- redim(grid, var = TRUE)
+      n.var <- getShape(grid, "var")
+      if ((n.var) != 1){
+        stop("For lamb, only 'psl' variable is required Use subsetGrid to extract it")
+      }
+      if (!is.null(centers)) {
+        message("Lamb WT was choosen, so the number of clusters will be forced to 27. Arg. 'centers' will be ignored.")
+      }
+      centers <- 27
+      arg.list <- list(...)
+      arg.list[["grid"]] <- grid
+      lamb.wt <- do.call("lambWT", arg.list)
+      #clusters.list <- vector("list", n.mem)
+      #for (m in 1:n.mem){
+        clusters.list <- array3Dto2Dmat(lamb.wt[[1]][[1]][[1]]$pattern)
+      #} 
+    } else { #Rest of clustering algorithms
+      #Scaling and combining data from all variables:
+      if (!(is.na(n.var))){
+        data.combined <- comb.vars(grid = grid, base = NULL, ref = NULL, var.names = var.names)
+      }else {
+        n.var <- 1
+        var <- var.names
+        data.combined <- comb.vars(grid = grid, base = NULL, ref = grid, var.names = var.names)
+      }
+      #Clustering Analysis of 'grid':
+      clusters.list <- clusterGrid_2D(grid.2D = data.combined, type, centers, ...)
+      centers <- nrow(clusters.list)
     }
-    centers <- 27
-    arg.list <- list(...)
-    arg.list[["grid"]] <- grid
-    lamb.wt <- do.call("lambWT", arg.list)
-    clusters.list <- vector("list", n.mem)
-    for (m in 1:n.mem){
-      clusters.list[[m]] <- array3Dto2Dmat(lamb.wt[[1]][[m]][[1]]$pattern)
-    } 
-  } else {
-    #Scaling and combining data from all variables by members:
-    if (!(is.na(n.var))){
-      data.combined <- comb.vars(grid = grid, base = NULL, ref = NULL, n.mem = n.mem, var.names = var.names)
-    }else {
-      n.var <- 1
-      var <- var.names
-      data.combined <- comb.vars(grid = grid, base = NULL, ref = grid, n.mem = n.mem, var.names = var.names)
+    if (type == "lamb") {
+      wt.index <- lamb.wt[[1]][[1]][[1]]$index
+    } else {
+      wt.index <- attr(clusters.list, "index")
     }
     
-    #Clustering Analysis of 'grid' by members:
-    clusters.list <- vector("list", length(data.combined))
-    for (m in 1:n.mem){
-      clusters.list[[m]] <- clusterGrid_2D(grid.2D = data.combined[[m]], type, centers, ...)
+    ### Weather types
+    if (is.null(y)){ 
+      out.grid <- grid
+    } else {
+      checkTemporalConsistency(grid, y)
+      out.grid <- y
     }
-    centers <- nrow(clusters.list[[1]])
-  }
-  
-  #Clustering Analysis of 'newdata':
-  if (!is.null(newdata)){
+    attr(out.grid, "cluster.type") <- type
+    attr(out.grid, "centers") <- centers
+    attr(out.grid, "wt.index") <- wt.index
+    attr(out.grid, "centroids") <- clusters.list[ , ]
+    if (type == "kmeans") {
+        attr(out.grid, "withinss") <- attr(clusters.list, "withinss")
+        attr(out.grid, "betweenss") <- attr(clusters.list, "betweenss")
+    }else if (type == "hierarchical") {
+        attr(out.grid, "height") <- attr(clusters.list, "height")
+        attr(out.grid, "cutree.at.height") <- attr(clusters.list, "cutree.at.height")
+        attr(out.grid, "diff.height.threshold") <- attr(clusters.list, "diff.height.threshold")
+    }
+  } else {
+    #Clustering Analysis of 'newdata':
+    if(is.null(attr(grid, "wt.index"))){
+      stop("'grid' is not a clustering object.")
+    }
     #Checking grid dimensions for newdata and var.names/n.mem matches with grid
-    newdata<-redim(newdata, member = TRUE)
-    newvar.names <- getVarNames(newdata)
-    if (length(match(newvar.names, var.names)) != n.var){ 
-      stop("Variables in 'newdata' don't match with variables in 'grid'")
+    if (getShape(newdata, "member") > 1){
+      message("Clustering analysis will be done after Ensemble mean...")
+      newdata <- suppressMessages(aggregateGrid(grid = newdata, aggr.mem = list(FUN = "mean", na.rm = TRUE)))
     }
-    if (getShape(newdata, "member") != n.mem){
-      stop("Number of members in 'newdata' is not the same as in 'grid'")
-    }
+    checkVarNames(newdata, grid)
+    checkDim(newdata, grid, dimensions = "var")
+    n.var <- suppressMessages(getShape(grid, "var")) 
     #Pre-processing in order to do clustering to ref CT's: 
     arg.list <- list(...)
     base <- arg.list[["base"]]
-    if (length(match(newvar.names, getVarNames(base))) != n.var){ 
-      stop("Variables in 'base' don't match with variables in 'newdata'")
+    if (!is.null(base)){ 
+      if (getShape(base, "member") > 1){
+        base <- suppressMessages(aggregateGrid(grid = base, aggr.mem = list(FUN = "mean", na.rm = TRUE)))
+      }
+      checkVarNames(newdata, base)
+      checkDim(newdata, base, dimensions = "var")
     }
     if (n.var != 1){
-      mat.newdata <- comb.vars(grid = newdata, base = base, ref = NULL, n.mem = getShape(newdata, "member"), var.names = getVarNames(newdata))
+      mat.newdata <- comb.vars(grid = newdata, base = base, ref = NULL, var.names = getVarNames(newdata))
     } else {
-      mat.newdata <- comb.vars(grid = newdata, base = base, ref = newdata, n.mem = getShape(newdata, "member"), var.names = getVarNames(newdata))
+      mat.newdata <- comb.vars(grid = newdata, base = base, ref = newdata, var.names = getVarNames(newdata))
     }
-    ind.list <- vector("list", length(mat.newdata))
-    for (m in 1:n.mem){
-      dist.mat <- rdist(mat.newdata[[m]],  clusters.list[[m]])
-      ind.list[[m]] <- apply(dist.mat, MARGIN = 1, FUN ="which.min")
-    }
+    dist.mat <- rdist(mat.newdata, attr(grid, "centroids"))
+    wt.index <- apply(dist.mat, MARGIN = 1, FUN ="which.min")
     if (is.null(y)){
-    out.grid <- newdata
+      out.grid <- newdata
+    } else {
+      checkTemporalConsistency(newdata, y)
+      out.grid <- y
     }
-  } else {
-    for (m in 1:n.mem){
-      if (type == "lamb") {
-        ind.list[[m]] <- lamb.wt[[1]][[m]][[1]]$index
-      } else {
-        ind.list[[m]] <- attr(clusters.list[[m]], "index")
-      }
-    }
-    if (is.null(y)){ 
-      out.grid <- grid
+    attr(out.grid, "cluster.type") <- type
+    attr(out.grid, "centers") <- centers
+    attr(out.grid, "wt.index") <- wt.index
+    attr(out.grid, "centroids") <- attr(grid, "centroids")
+    if (type == "kmeans") {
+      attr(out.grid, "withinss") <- attr(grid, "withinss")
+      attr(out.grid, "betweenss") <- attr(grid, "betweenss")
+    }else if (type == "hierarchical") {
+      attr(out.grid, "height") <- attr(grid, "height")
+      attr(out.grid, "cutree.at.height") <- attr(grid, "cutree.at.height")
+      attr(out.grid, "diff.height.threshold") <- attr(grid, "diff.height.threshold")
     }
   }
-
-
-  if (!is.null(y)){
-    ### Weather types
-    if (!is.null(newdata)){
-      aux <- intersectGrid.time(y, newdata) 
-      time.size <- getShape(aux, dimension = "time")
-      if (time.size != getShape(newdata, dimension = "time")){
-        stop("'newdata' and 'y' time dimensions' don't match. Don't use that 'y' in this case...")
-      } 
-    }else {
-      aux <- intersectGrid.time(y, grid) 
-      time.size <- getShape(aux, dimension = "time")
-      if (time.size != getShape(grid, dimension = "time")){
-        stop("'newdata' and 'y' time dimensions' don't match. Don't use that 'y' in this case...")
-      } 
-    }
-    out.grid <- y
-  }
-  attr(out.grid, "cluster.type") <- type
-  attr(out.grid, "centers") <- centers
-  for (m in 1:n.mem){
-    index.mat <- rbind(ind.list[[m]])
-    centroids <- rbind(clusters.list[[m]][ , ])
-    withinss <- rbind(attr(clusters.list[[m]], "withinss"))
-    betweenss <- rbind(attr(clusters.list[[m]], "betweenss"))
-    height <- rbind(attr(clusters.list[[m]], "height"))
-    cutree.at.height <- rbind(attr(clusters.list[[m]], "cutree.at.height"))
-    if (m == 1) {diff.height.threshold <- rbind(attr(clusters.list[[m]], "diff.height.threshold"))}
-  }
-  attr(out.grid, "index") <- drop(index.mat) 
-  attr(out.grid, "centroids") <-  centroids
-  if (type == "kmeans") {
-    attr(out.grid, "withinss") <- withinss
-    attr(out.grid, "betweenss") <- betweenss
-  }else if (type == "hierarchical") {
-    attr(out.grid, "height") <- height
-    attr(out.grid, "cutree.at.height") <- cutree.at.height
-    attr(out.grid, "diff.height.threshold") <- diff.height.threshold
-  }
+  
   return(out.grid)
 }
 
 
 #' @title Variables combination
-#' @description Scaling and combining data from all variables by members
+#' @description Scaling and combining data from all variables
 #' @param grid A grid containing the variables to be scaled and combined 
 #' @param base Reference baseline grid to be passed to \code{'scaleGrid'}. See \link{scaleGrid} for further information.
 #' @param ref Reference grid to be passed to \code{'scaleGrid'}. See \link{scaleGrid} for further information.
-#' @param n.mem Number of members 
 #' @param var.names Variables names
 #' @importFrom magrittr %>%  
 #' @return A list
 #' @keywords internal
 #' @author J. A. Fernandez
 
-comb.vars <- function(grid, base, ref, n.mem, var.names){
+comb.vars <- function(grid, base, ref, var.names){
   grid.scaled <- suppressMessages(scaleGrid(grid = grid, base = base, ref = ref, type = "standardize"))
-  data.combined <- lapply(1:n.mem, function(m) {
-    l <- subsetGrid(grid.scaled, members = m) %>% redim(var = TRUE)
-    vardata.list <- lapply(1:length(var.names), function(x) {
-      suppressMessages(subsetGrid(l, var = var.names[x])[["Data"]]) %>% array3Dto2Dmat()
-    }) 
-    do.call("cbind", vardata.list)
-  })
+  l <- redim(grid.scaled, var = TRUE)
+  vardata.list <- lapply(1:length(var.names), function(x) {
+    suppressMessages(subsetGrid(l, var = var.names[x])[["Data"]]) %>% array3Dto2Dmat()
+  }) 
+  data.combined <- do.call("cbind", vardata.list)
   return(data.combined)
 }
 
