eb4c28626736cd16ca8614284c306dd8b829d8c1
jbedia <joaquin.bedia@gmail.com>
2020-05-22 20:41:31 +0200

    Remove internal world lines layer (moved to visualizeR)

--- R/helpers.R
+++ R/helpers.R
@@ -300,29 +300,6 @@ getShape <- function(obj, dimension = NULL) {
       return(shape)
 }
 
-
-#' @title Land borders
-#' @description Add land borders to a map
-#' @param ... Graphical parameters passed to \code{\link{lines}}.
-#' @return Draws a simplied land border areas as lines onto the map
-#' @details The function loads a built-in world segments dataset created ad hoc to avoid dependencies on other packages (i.e. 'maps').
-#' Geographical lonlat coordinates in wgs84.
-#' @source Postprocessed from the original shapefile from Natural Earth (http://www.naturalearthdata.com/downloads/110m-physical-vectors/)
-#' @author J. Bedia
-#' @keywords internal
-#' @importFrom graphics lines
-#' @export
-
-draw.world.lines <- function(...) {
-      load(system.file(package = "transformeR","wrl.Rda"), envir = environment())
-      for (i in 1:length(node.list)) {
-            lines(node.list[[i]][,1], node.list[[i]][,2], ...)            
-      }
-}
-
-
-
-
 #' @title Leap years
 #' @description Return the indices of leap years in a vector of years
 #' @param years a integer vector of (gregorian) years
